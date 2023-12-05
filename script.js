bb5341a3f1840c63189141c406e83ffc5e306885
shor-oth <105999022+shor-oth@users.noreply.github.com>
2023-12-05 13:59:19 -0500

    first round of edits

---
+++ script.js
@@ -0,0 +1,31 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds (adjust as needed)
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    const slides = document.getElementsByClassName("slide");
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
